03a0aada85d117c4b58ac0950241c1e6a8f2a0b1
Aidi Stan <aidistan@live.com>
2015-08-12 15:24:19 +0800

    Reimplement adding userId for items

--- common/models/gallery.js
+++ common/models/gallery.js
@@ -1,7 +1,3 @@
 module.exports = function(Gallery) {
-  // Create userId for items
-  Gallery.beforeRemote('prototype.__create__items', function(ctx, item, next) {
-    ctx.req.body.userId = ctx.req.accessToken.userId;
-    next();
-  });
+
 };
--- common/models/item.js
+++ common/models/item.js
@@ -1,3 +1,19 @@
 module.exports = function(Item) {
-
+  // Add userId for items
+  Item.on('dataSourceAttached', function(obj){
+    var context = this;
+    var create  = Item.create;
+    Item.create = function(data, options, cb) {
+      Item.app.models.Gallery.findById(data.galleryId, function(err, obj) {
+        if (err || obj == null) {
+          cb(err, obj);
+        } else {
+          data.userId = obj.userId;
+          create.apply(Item, [data, options, function(err, obj) {
+            return(cb(err, obj));
+          }]);
+        }
+      });
+    };
+  });
 };
