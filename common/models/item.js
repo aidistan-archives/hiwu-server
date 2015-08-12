ef5ee408197d0bbd125596e77ae6db15fc0dab56
Aidi Stan <aidistan@live.com>
2015-08-13 00:42:42 +0800

    Upload files as multipart/form-data

--- common/models/item.js
+++ common/models/item.js
@@ -1,7 +1,11 @@
+var multiparty = require('multiparty');
+var fs = require('fs');
+
 module.exports = function(Item) {
-  // Add userId for items
+  // Wrap the built-in `create` method to add userId attribute
+  // Refer to the example in "Change the implementation of built-in methods"
+  // http://docs.strongloop.com/display/public/LB/Customizing+models
   Item.on('dataSourceAttached', function(obj){
-    var context = this;
     var create  = Item.create;
     Item.create = function(data, options, cb) {
       Item.app.models.Gallery.findById(data.galleryId, function(err, obj) {
@@ -16,4 +20,53 @@ module.exports = function(Item) {
       });
     };
   });
+
+  // Cancel the built-in `__create__photos` method
+  Item.disableRemoteMethod('__create__photos', false);
+
+  // Define the `create_photo` method
+  Item.prototype.create_photo = function(req, cb) {
+    new multiparty.Form().parse(req, function(err, data, files) {
+      if (err) cb(err, null);
+
+      data.itemId = req.remotingContext.instance.id;
+      Item.app.models.Photo.create(data, function(err, photo) {
+        var oss  = Item.app.aliyun.oss;
+        var file = files.data[0];
+        if (!err) {
+          // Update the url
+          photo.updateAttribute('url', oss.makeUrl('photo', photo.id));
+
+          // Save the image
+          oss.putObject({
+            Bucket: 'hiwu',
+            Key: oss.makeKey('photo', photo.id),
+            Body: fs.readFileSync(file.path),
+            ContentType: file.headers['content-type'],
+          }, function (err, data) {
+            if (err) {
+              console.log('Error raised when uploading a photo to Aliyun OSS:', err);
+            }
+
+            // Cleanup after uploading
+            for (i in files) for (j in files[i])
+              fs.unlink(files[i][j].path, function (err) { if (err) throw err; });
+          });
+        }
+        cb(err, photo);
+      });
+    });
+  }
+
+  // Register the `upload_photo` method
+  Item.remoteMethod(
+    'create_photo',
+    {
+      description: 'Upload a new photo to this item.',
+      accepts: {arg: 'req', type: 'object', http: { source: 'req' }},
+      returns: {arg: 'photo', type: 'Photo', root: true},
+      http: {verb: 'post', path: '/photos'},
+      isStatic: false
+    }
+  );
 };
--- common/models/photo.js
+++ common/models/photo.js
@@ -1,47 +1,6 @@
 var url = require('url')
 
 module.exports = function(Photo) {
-  // Upload the data to Aliyun OSS after creation
-  Photo.on('dataSourceAttached', function(obj){
-    var create = Photo.create;
-    Photo.create = function(data, options, cb) {
-      var oss = Photo.app.aliyun.oss;
-
-      if (data.data) {
-        var bin = new Buffer(data.data, 'binary');
-        delete data.data;
-      } else {
-        cb(new Error('photo.data not given'), data);
-      }
-
-      if (data.type) {
-        var type = data.type;
-        delete data.type;
-      } else {
-        cb(new Error('photo.type not given'), data);
-      }
-
-      create.apply(this, [data, options, function(err, obj) {
-        if (!err) {
-          // Update the url
-          obj.updateAttribute('url', oss.makeUrl('photo', obj.id));
-          // Save the image
-          oss.putObject({
-            Bucket: 'hiwu',
-            Key: oss.makeKey('photo', obj.id),
-            Body: bin,
-            ContentType: type,
-          }, function (err, data) {
-            if (err) {
-              console.log('Error raised when uploading a photo to Aliyun OSS:', err);
-            }
-          });
-        }
-        return(cb(err,obj));
-      }]);
-    };
-  });
-
   // Delete the data from Aliyun OSS before deletion
   Photo.observe('before delete', function(ctx, next) {
     Photo.find({ where: ctx.where }, function(err, photos) {
