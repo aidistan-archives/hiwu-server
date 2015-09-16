741994b783b2bcba18cded1a8ec716d03e990bf8
Aidi Stan <aidistan@live.com>
2015-09-16 22:24:16 +0800

    Implement admin role

---
+++ server/boot/1-admin.js
@@ -0,0 +1,19 @@
+module.exports = function(app) {
+
+  app.models.HiwuUser.create({
+    username: 'hiwu.ren', email: 'hiwu.ren@hiwu.ren', password: 'duludou!'
+  }, function(err, user) {
+      if (err) return;
+
+      app.models.Role.create({
+        name: 'admin'
+      }, function(err, role) {
+        if (err) return;
+
+        role.principals.create({
+          principalType: app.models.RoleMapping.USER,
+          principalId: user.id
+        }, function(err, principal) {});
+      });
+  });
+};
