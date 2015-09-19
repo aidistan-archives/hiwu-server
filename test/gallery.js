bb782728f61d9c3e9f2cf2f0b30303d4fa3e1b3d
Aidi Stan <aidistan@live.com>
2015-09-19 11:27:47 +0800

    Implement Gallery#publicView

---
+++ test/gallery.js
@@ -0,0 +1,86 @@
+var HiwuApi = require('./hiwu-api-lib');
+var async = require('async');
+var assert = require('assert');
+var spawn = require('child_process').spawn;
+
+describe('HiwuApi', function() {
+  describe('Gallery', function () {
+    var server;
+
+    beforeEach(function(done) {
+      server = spawn('node', ['.']);
+
+      function complete(data) {
+        server.stdout.removeListener('data', complete);
+        done();
+      }
+
+      server.stdout.on('data', complete);
+    });
+
+    afterEach(function(done) {
+      server.kill();
+      done();
+    });
+
+    describe('#publicView', function(done) {
+      it('should not return if private', function(done) {
+        var api = new HiwuApi();
+
+        async.series([
+          function(cb) {
+            api.HiwuUser.login({
+              username: 'hiwu.ren',
+              password: 'duludou!'
+            }, 'user', cb);
+          },
+          function(cb) {
+            api.HiwuUser.createGallery(api.lastResult.user.id, {
+              name: 'Gallery',
+              public: false
+            }, cb);
+          }
+        ], function(err, results) {
+          api.Gallery.publicView(api.lastResult.id, function(err, gallery) {
+            assert(gallery.error);
+            assert.equal('PRIVATE_MODEL_VISITED', gallery.error.code);
+            done();
+          });
+        });
+      });
+
+      it('should return if public', function(done) {
+        var api = new HiwuApi();
+        var gallery;
+
+        async.series([
+          function(cb) {
+            api.HiwuUser.login({
+              username: 'hiwu.ren',
+              password: 'duludou!'
+            }, 'user', cb);
+          },
+          function(cb) {
+            api.HiwuUser.createGallery(api.lastResult.user.id, {
+              name: 'Gallery'
+            }, cb);
+          },
+          function(cb) {
+            gallery = api.lastResult;
+            api.Gallery.createItem(gallery.id, {
+              name: 'Item'
+            }, cb);
+          }
+        ], function(err, results) {
+          api.Gallery.publicView(gallery.id, function(err, gallery) {
+            assert(gallery.id);
+            assert.equal('Gallery', gallery.name);
+            assert(gallery.hiwuUser.id);
+            assert.equal('Item', gallery.items[0].name);
+            done();
+          });
+        });
+      });
+    });
+  });
+});
--- test/hiwu-api-lib/gallery.js
+++ test/hiwu-api-lib/gallery.js
@@ -25,7 +25,28 @@ Gallery.prototype = {
       api.lastResult = res.body;
       if (cb) cb(err, res.body); else api.end();
     });
-  }
+  },
+
+  publicView: function(galleryId, cb) {
+    var api = this.api;
+    var path = '/api/Galleries/' + galleryId + '/publicView';
+
+    if (api.debugger.border)
+      console.log('====================');
+    if (api.debugger.api)
+      console.log('GET: ' + path);
+    path += '?access_token=' + api.accessToken.id;
+
+    needle.get(api.url(path), null, function(err, res) {
+      if (err) throw err;
+      if (api.debugger.status)
+        console.log('STATUS: ' + res.statusCode);
+      if (api.debugger.body)
+        console.log('BODY: ' + JSON.stringify(res.body));
+      api.lastResult = res.body;
+      if (cb) cb(err, res.body); else api.end();
+    });
+  },
 };
 
 module.exports = Gallery;
