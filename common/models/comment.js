706f5dca1e7c6c3bdcbfe0b58c8bcf49e5215a76
Aidi Stan <aidistan@live.com>
2015-09-20 12:59:22 +0800

    Implement Notifications

--- common/models/comment.js
+++ common/models/comment.js
@@ -1,3 +1,34 @@
-module.exports = function(Comment) {
+var loopback = require('loopback');
 
+module.exports = function(Comment) {
+  Comment.on('dataSourceAttached', function(obj){
+    var create  = Comment.create;
+    Comment.create = function(data, options, cb) {
+      create.apply(Comment, [data, options, function(err, comment) {
+        if (err) cb(err);
+        Comment.app.models.Item.findById(comment.itemId, {
+           fields: ['userId']
+        }, function(err, item) {
+          if (err) cb(err);
+
+          var fromId = loopback.getCurrentContext().get('accessToken').userId;
+          var userId = comment.toId || item.userId;
+
+          if (fromId === userId) {
+            cb(err, comment);
+          } else {
+            Comment.app.models.Notification.create({
+              type: comment.toId ? 'COMMENT_REPLY' : 'ITEM_COMMENT',
+              userId: userId,
+              fromId: fromId,
+              itemId: comment.itemId,
+              commentId: comment.id
+            }, {}, function(err, notification) {
+              cb(err, comment);
+            });
+          }
+        });
+      }]);
+    };
+  });
 };
--- common/models/like.js
+++ common/models/like.js
@@ -1,3 +1,33 @@
-module.exports = function(CollectionItem) {
+var loopback = require('loopback');
 
+module.exports = function(Like) {
+  Like.on('dataSourceAttached', function(obj){
+    var create  = Like.create;
+    Like.create = function(data, options, cb) {
+      create.apply(Like, [data, options, function(err, like) {
+        if (err) cb(err);
+        Like.app.models.Item.findById(like.itemId, {
+           fields: ['userId']
+        }, function(err, item) {
+          if (err) cb(err);
+
+          var fromId = loopback.getCurrentContext().get('accessToken').userId;
+          var userId = item.userId;
+
+          if (fromId ===  userId) {
+            cb(err, like);
+          } else {
+            Like.app.models.Notification.create({
+              type: 'ITEM_LIKE',
+              userId: userId,
+              fromId: fromId,
+              itemId: like.itemId
+            }, {}, function(err, notification) {
+              cb(err, like);
+            });
+          }
+        });
+      }]);
+    };
+  });
 };
