c8b580865cbbbd6082820c65caab012ec7834560
Aidi Stan <aidistan@live.com>
2015-09-20 11:27:18 +0800

    Refactor test/publicView.js

--- test/publicView.js
+++ test/publicView.js
@@ -4,7 +4,9 @@ var assert = require('assert');
 var needle = require('needle');
 var spawn = require('child_process').spawn;
 
-describe('Integration Test: views from public', function () {
+describe('Integration Test: public views', function () {
+  this.timeout(3000);
+
   var api;
   var server;
 
@@ -13,6 +15,100 @@ describe('Integration Test: views from public', function () {
   var publicGallery;
   var privateItem;
   var publicItem;
+
+  before(function(done) {
+    server = spawn('node', ['.']);
+
+    function complete(data) {
+      server.stdout.removeListener('data', complete);
+      init(done);
+    }
+
+    server.stdout.on('data', complete);
+  });
+
+  after(function(done) {
+    server.kill();
+    done();
+  });
+
+  describe('HiwuUser', function() {
+    describe('#publicView', function () {
+      before(function(done) {
+        api.HiwuUser.publicView(hiwuUser.id, function(err, res) {
+          hiwuUser = res;
+          done();
+        });
+      });
+
+      it('should return public galleries', function() {
+        assert.equal(1, hiwuUser.galleries.length);
+        testPublicGallery(hiwuUser.galleries[0]);
+      });
+    });
+  });
+
+  describe('Gallery', function() {
+    describe('#publicView', function () {
+      it('should not return if private', function(done) {
+        api.Gallery.publicView(privateGallery.id, function(err, gallery) {
+          assert(gallery.error);
+          assert.equal('PRIVATE_MODEL_VISITED', gallery.error.code);
+          done();
+        });
+      });
+
+      it('should return if public', function(done) {
+        api.Gallery.publicView(publicGallery.id, function(err, gallery) {
+          assert(gallery.hiwuUser);
+          testPublicGallery(gallery);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('Item', function() {
+    describe('#publicView', function () {
+      it('should not return if private', function(done) {
+        api.Item.publicView(privateItem.id, function(err, item) {
+          assert(item.error);
+          assert.equal('PRIVATE_MODEL_VISITED', item.error.code);
+          done();
+        });
+      });
+
+      it('should return if public', function(done) {
+        api.Item.publicView(publicItem.id, function(err, item) {
+          assert(item.hiwuUser);
+          testPublicItem(item);
+          assert(item.likes !== undefined);
+          assert(item.liked !== undefined);
+          assert(item.comments);
+          done();
+        });
+      });
+    });
+  });
+
+  describe('Today', function () {
+    describe('#publicView', function () {
+      var galleries;
+
+      before(function(done) {
+        api.Today.publicView(function(err, res) {
+          galleries = res;
+          done();
+        });
+      });
+
+      it('should return public galleries with related models', function() {
+        assert.equal(1, galleries.length);
+        assert(galleries[0].hiwuUser);
+        testPublicGallery(galleries[0]);
+      });
+    });
+  });
 
   function init(cb) {
     api = new HiwuApi();
@@ -68,128 +164,15 @@ describe('Integration Test: views from public', function () {
     ], cb);
   }
 
-  before(function(done) {
-    server = spawn('node', ['.']);
+  function testPublicGallery(gallery) {
+    assert(gallery.public);
+    assert(gallery.items);
+    for(var i in gallery.items)
+      testPublicItem(gallery.items[i]);
+  }
 
-    function complete(data) {
-      server.stdout.removeListener('data', complete);
-      init(done);
-    }
-
-    server.stdout.on('data', complete);
-  });
-
-  after(function(done) {
-    server.kill();
-    done();
-  });
-
-  describe('HiwuUser', function() {
-    describe('#publicView', function () {
-      before(function(done) {
-        api.HiwuUser.publicView(hiwuUser.id, function(err, res) {
-          hiwuUser = res;
-          done();
-        });
-      });
-
-      it('should return public galleries', function() {
-        assert.equal(1, hiwuUser.galleries.length);
-        assert(hiwuUser.galleries[0].public);
-        assert(hiwuUser.galleries[0].items);
-      });
-
-      it('should return public items', function() {
-        var items = hiwuUser.galleries[0].items;
-
-        assert.equal(1, items.length);
-        assert(items[0].public);
-        assert(items[0].photos);
-      });
-    });
-  });
-
-  describe('Gallery', function() {
-    describe('#publicView', function () {
-      it('should not return if private', function(done) {
-        api.Gallery.publicView(privateGallery.id, function(err, gallery) {
-          assert(gallery.error);
-          assert.equal('PRIVATE_MODEL_VISITED', gallery.error.code);
-          done();
-        });
-      });
-
-      it('should return if public', function(done) {
-        api.Gallery.publicView(publicGallery.id, function(err, gallery) {
-          assert.equal(publicGallery.id, gallery.id);
-          assert.equal('Public Gallery', gallery.name);
-          assert(gallery.hiwuUser);
-          assert.equal('Public Item', gallery.items[0].name);
-          done();
-        });
-      });
-
-      it('should return with public items', function(done) {
-        api.Gallery.publicView(publicGallery.id, function(err, gallery) {
-          assert.equal(1, gallery.items.length);
-          assert.equal('Public Item', gallery.items[0].name);
-          done();
-        });
-      });
-    });
-  });
-
-  describe('Item', function() {
-    describe('#publicView', function () {
-      it('should not return if private', function(done) {
-        api.Item.publicView(privateItem.id, function(err, item) {
-          assert(item.error);
-          assert.equal('PRIVATE_MODEL_VISITED', item.error.code);
-          done();
-        });
-      });
-
-      it('should return if public', function(done) {
-        api.Item.publicView(publicItem.id, function(err, item) {
-          assert.equal(publicItem.id, item.id);
-          assert.equal('Public Item', item.name);
-          assert(item.hiwuUser);
-          assert(item.likes === 0);
-          assert(!item.liked);
-          assert(item.photos);
-          done();
-        });
-      });
-    });
-  });
-
-  describe('Today', function () {
-    describe('#publicView', function () {
-      var galleries;
-
-      before(function(done) {
-        api.Today.publicView(function(err, res) {
-          galleries = res;
-          done();
-        });
-      });
-
-      it('should return public galleries with related models', function() {
-        assert.equal(1, galleries.length);
-
-        var gallary = galleries[0];
-        assert(gallary.public);
-        assert(gallary.hiwuUser);
-        assert(gallary.items);
-      });
-
-      it('should return public items with related models', function() {
-        assert.equal(1, galleries[0].items.length);
-
-        var item = galleries[0].items[0];
-        assert(item.public);
-        assert(item.photos);
-      });
-    });
-  });
+  function testPublicItem(item) {
+    assert(item.public);
+    assert(item.photos);
+  }
 });
