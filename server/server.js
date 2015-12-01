30a466d25902ef8d90949164c9ba2c978765f69c
Aidi Stan <aidistan@live.com>
2015-12-01 19:25:54 +0800

    Disable explorer in production

--- server/server.js
+++ server/server.js
@@ -21,7 +21,14 @@ app.start = function() {
 
 // Bootstrap the application, configure models, datasources and middleware.
 // Sub-apps like REST API are mounted via boot scripts.
-boot(app, __dirname, function(err) {
+boot(app, {
+  appRootDir: __dirname,
+  components: app.get('env') === 'production' ? {} : {
+    'loopback-component-explorer': {
+      'mountPath': '/explorer'
+    }
+  }
+}, function(err) {
   if (err) throw err;
 
   // start the server if `$ node server.js`
